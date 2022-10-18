8557d66b394e1703b482eaaec7d0654d7d3dfd61
ms0130k <minseok0130@gmail.com>
2022-10-19 08:27:43 +0900

    update user 추가

---
+++ src/main/java/next/web/UpdateUserServlet.java
@@ -0,0 +1,46 @@
+package next.web;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.h2.engine.Database;
+
+import core.db.DataBase;
+import next.model.User;
+
+@WebServlet("/user/update")
+public class UpdateUserServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String userId = req.getParameter("userId");
+        User user = DataBase.findUserById(userId);
+        req.setAttribute("user", user);
+        RequestDispatcher rd = req.getRequestDispatcher("/user/update.jsp");
+        rd.forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String userId = req.getParameter("userId");
+        String name = req.getParameter("name");
+        String email = req.getParameter("email");
+        
+        User user = DataBase.findUserById(userId);
+        if (user == null) {
+            resp.sendRedirect("/user/list");
+            return;
+        }
+        user.setName(name);
+        user.setEmail(email);
+        
+        resp.sendRedirect("/user/list");
+    }
+}
